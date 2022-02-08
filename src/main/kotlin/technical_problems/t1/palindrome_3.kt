def3a29d2ab082099ced20bc80ea348d52ecb191
Ralph Cajipe <ralphcajipe@gmail.com>
2022-02-08 22:18:44 +0800

    Add new palindrome and new highest_num

---
+++ src/main/kotlin/technical_problems/t1/palindrome_3.kt
@@ -0,0 +1,17 @@
+package technical_problems.t1
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val userInput = Scanner(System.`in`)
+    var s1: String
+    var reverse: String = ""
+    s1 = userInput.nextLine()
+    var l: Int = s1.length - 1
+
+    while (l != -1) {
+        reverse += s1[l--];
+    }
+    if (s1 == reverse) println("$s1 is a palindrome")
+    else println("$s1 is not a palindrome")
+}
