7b75fce821f9f47e70a4220ac1c33384009035f0
Ralph Cajipe <ralphcajipe@gmail.com>
2022-02-18 23:51:15 +0800

    Refactor tp02

--- src/main/kotlin/technical_problems/tp02/function_fibo.kt
+++ src/main/kotlin/technical_problems/tp02/function_fibo.kt
@@ -4,6 +4,7 @@ package technical_problems.tp02
  *  Function Problem: Fibonacci Sequence
  *  The Fibonacci sequence is a series of numbers where
  *  a number is the addition of the last two numbers, starting with 0, and 1.
+ *
  *  The Fibonacci Sequence: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55…
  *
  *  Written as a rule, the expression is:
@@ -11,34 +12,66 @@ package technical_problems.tp02
  *
  *  Complete the given program code that will compute
  *  the sum of the terms of the Fibonacci sequence.
+ *  #################################################
+ *  fun main(){
+ *       print("Enter number of Terms: ")
+ *       var n: Int? = readLine()?.toIntOrNull()
+ *       // some code
+ *  }
+ *
+ *  fun fiboSum(n:Int): Int{
+ *  // fibonacci code
+ *  }
+ *  #################################################
+ *  TEST CASE:
+ *
+ *  Example Output 1
+ *  Enter number of Terms: 5
+ *  0 1 1 2 3 The sum is 7
+ *
+ *  Example Output 2
+ *  Enter number of Terms: 9
+ *  0 1 1 2 3 5 8 13 21 The sum is 54
+ *
+ *  Example Output 3
+ *  Enter number of Terms: -8
+ *  Invalid Input
+ *
+ *  Example Output 4
+ *  Enter number of Terms: one
+ *  Invalid Input
+ *
+ *  Example Output 5
+ *  Enter number of Terms: 1.25
+ *  Invalid Input
  */
 
 fun fiboSum(n: Int): Int {
     var t1 = 0
     var t2 = 1
-    var res = 0
+    var fiboResult = 0
 
     for (i in 1..n) {
         print("$t1 ")
-        res += t1
+        fiboResult += t1
         val sum = t1 + t2
         t1 = t2
         t2 = sum
     }
-    return res
+    return fiboResult
 }
 
 
 fun main() {
     print("Enter number of Terms: ")
     val n: Int? = readLine()?.toIntOrNull()
-    // 	var n = 45
+    // 	var n = 9
     //  print("$n")
     if (n == null || n < 1) {
         println("Invalid Input")
     } else {
-        val res = fiboSum(n)
-        println("The sum is $res")
+        val fiboResult = fiboSum(n)
+        println("The sum is $fiboResult")
     }
 
 }
--- src/main/kotlin/technical_problems/tp02/lambda.kt
+++ src/main/kotlin/technical_problems/tp02/lambda.kt
@@ -4,23 +4,64 @@ package technical_problems.tp02
  *  Lambda Expression Problem:
  *  Complete the given program code that will ask the user to enter
  *  some string and display the vowels and consonants characters
+ *  ################################################################
+ *  fun main() {
+ *      print("Enter some string: ")
+ *      val str = rdLn()
+ *
+ *      val vowels = str.filter {
+ *      // some code here
+ *      }
+ *
+ *      val consonants = str.filter {
+ *      //some code here
+ *      }
+ *
+ *      println("Vowels: $vowels")
+ *      println("Consonants: $consonants")
+ *  }
+ *
+ *  private fun rdLn() = readLine()!!
+ *  private fun rdInt() = rdLn().toInt()
+ *  #################################################################
+ *  TEST CASE:
+ *
+ *  Example Output 1
+ *  Enter some string: the quick brown fox
+ *  Vowels: euioo
+ *  Consonants: thqckbrwnfx
+ *
+ *  Example Output 2
+ *  Enter some string: There are 56 students enrolled
+ *  Vowels: eeaeueeoe
+ *  Consonants: Thrrstdntsnrlld
+ *
+ *  Example Output 3
+ *  Enter some string: underscore(_) asterisk(*) and hashtag(#)
+ *  Vowels: ueoeaeiaaa
+ *  Consonants: ndrscrstrskndhshtg
  */
+
+
 fun main() {
     print("Enter some string: ")
     val str = readLine()
-    val vowels = str?.filter {
-            it -> (it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u'
+
+    // Check if input string has vowels
+    val vowels = str?.filter { it ->
+        (it == 'a' || it == 'e' || it == 'i' || it == 'o' || it == 'u'
                 || it == 'A' || it == 'E' || it == 'I' || it == 'O' || it == 'U')
     }
 
-    val constants = str?.filter {
-            it -> (it != 'a' && it != 'e' && it != 'i' && it != 'o' && it != 'u'
+    // Check if input string doesn't have vowels, we're after the consonants
+    val consonants = str?.filter { it ->
+        (it != 'a' && it != 'e' && it != 'i' && it != 'o' && it != 'u'
                 && it != 'A' && it != 'E' && it != 'I' && it != 'O' && it != 'U'
                 && (it in 'a'..'z' || it in 'A'..'Z')
                 && it.isLetter())
     }
 
     println("Vowels: $vowels")
-    println("Consonants: $constants")
+    println("Consonants: $consonants")
 
 }
